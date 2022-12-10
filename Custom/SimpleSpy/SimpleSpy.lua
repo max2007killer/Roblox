b67e56272e7781156b5374e7cf6b0637e8679f5f
James <91840948+KATERGaming@users.noreply.github.com>
2022-12-10 10:44:22 +0100

    Update SimpleSpy.lua

--- Custom/SimpleSpy/SimpleSpy.lua
+++ Custom/SimpleSpy/SimpleSpy.lua
@@ -15,7 +15,7 @@ local Players = game:GetService("Players")
 local CoreGui = game:GetService("CoreGui")
 local Highlight =
 	loadstring(
-		game:HttpGet("https://github.com/exxtremestuffs/SimpleSpySource/raw/master/highlight.lua")
+		game:HttpGet("https://raw.githubusercontent.com/KATERGaming/Roblox/main/Custom/SimpleSpy/highlight.lua")
 	)()
 
 ---- GENERATED (kinda sorta mostly) BY GUI to LUA ----
