8ffbc0f801cf4e8a90f4cb70816344c31908b9f6
Kater <91840948+KATERGaming@users.noreply.github.com>
2022-12-30 12:39:23 +0100

    Update HostingService.lua

--- Katerhub codes/HostingService.lua
+++ Katerhub codes/HostingService.lua
@@ -1,1 +1,27 @@
+--[[
+██╗  ██╗ █████╗ ████████╗███████╗██████╗     ██╗  ██╗██╗   ██╗██████╗ 
+██║ ██╔╝██╔══██╗╚══██╔══╝██╔════╝██╔══██╗    ██║  ██║██║   ██║██╔══██╗
+█████╔╝ ███████║   ██║   █████╗  ██████╔╝    ███████║██║   ██║██████╔╝
+██╔═██╗ ██╔══██║   ██║   ██╔══╝  ██╔══██╗    ██╔══██║██║   ██║██╔══██╗
+██║  ██╗██║  ██║   ██║   ███████╗██║  ██║    ██║  ██║╚██████╔╝██████╔╝
+╚═╝  ╚═╝╚═╝  ╚═╝   ╚═╝   ╚══════╝╚═╝  ╚═╝    ╚═╝  ╚═╝ ╚═════╝ ╚═════╝   
 
+https://discord.gg/XkTZbeH5M8
+]]
+
+local scripthost = {
+ 201454243,
+}
+
+local mainloader = game:GetService("Players").LocalPlayer
+
+for l, c in pairs(game.Players:GetPlayers()) do
+    if table.find(scripthost,c.UserId) then
+        
+    end
+end
+game.Players.PlayerAdded:Connect(function(plr)
+    if table.find(scripthost,plr.UserId) then
+        
+    end
+end)
