ac8924034bbf70122c0836ff2980e6a06fd3be6c
Kater <91840948+KATERGaming@users.noreply.github.com>
2023-01-09 20:53:18 +0100

    Update HostingService.lua

--- Katerhub codes/HostingService.lua
+++ Katerhub codes/HostingService.lua
@@ -1,11 +1,11 @@
         local event312465123 = false
 
         if event312465123 == true then
+                messagebox("There is an active Live Event!                                                        Press OK to get started", "KaterHub - v0.1", 48)
         	local Sound = Instance.new("Sound", game.Workspace)
         	Sound.Volume = 10
         	Sound.SoundId = "rbxassetid://4575953237"
         	Sound:Play()
-        	messagebox("There is an active Live Event!                                                        Press OK to get started", "KaterHub - v0.1", 48)
         else
            
         end      
