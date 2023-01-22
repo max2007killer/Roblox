b26440c033187de40d90889106c8fd26fa00a9b0
Kater <91840948+KATERGaming@users.noreply.github.com>
2023-01-22 22:17:44 +0100

    Update Show case.lua

--- RayField UI/Show case.lua
+++ RayField UI/Show case.lua
@@ -103,3 +103,18 @@ local Dropdown = Tab:CreateDropdown({
    -- The variable (Option) is a string for the value that the dropdown was changed to
    end,
 })
+
+local Keybind = Tab:CreateKeybind({
+   Name = "Keybind Example",
+   CurrentKeybind = "Q",
+   HoldToInteract = false,
+   Flag = "Keybind1", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
+   Callback = function(Keybind)
+   -- The function that takes place when the keybind is pressed
+   -- The variable (Keybind) is a boolean for whether the keybind is being held or not (HoldToInteract needs to be true)
+   end,
+})
+
+local Paragraph = Tab:CreateParagraph({Title = "Paragraph Example", Content = "Paragraph Example"})
+
+local Label = Tab:CreateLabel("Label Example")
