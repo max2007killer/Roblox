71626f035b51299782661a8eedf6a07afc39d053
Kater <91840948+KATERGaming@users.noreply.github.com>
2023-02-03 12:22:09 +0100

    Update config.lua

--- Chat-Spy/config.lua
+++ Chat-Spy/config.lua
@@ -1,18 +1,7 @@
---[[
-	Simple Chat Spy
-	Type "spy" to enable or disable the chat spy.
-	Only tested if this works executed with Synapse (should work with other exploits though)
---]]
-
-print("-- Chat Spy Executed --")
-print("Type \"spy\" to enable or disable the chat spy.")
-print("Only tested if this works executed with Synapse (should work with other exploits though)")
-print("https://github.com/dehoisted/Chat-Spy")
-
 -- Configuration
 Config = {
 	enabled = true,
-	spyOnMyself = true,
+	spyOnMyself = false,
 	public = false,
 	publicItalics = true
 }
