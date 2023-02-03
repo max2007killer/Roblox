d77dafec0579908c897d8710a64086bfd9c66299
Kater <91840948+KATERGaming@users.noreply.github.com>
2023-02-03 12:23:01 +0100

    Update Main.lua

--- Chat-Spy/Main.lua
+++ Chat-Spy/Main.lua
@@ -1,23 +1,10 @@
---[[
-	Simple Chat Spy
-	Type "spy" to enable or disable the chat spy.
-	Only tested if this works executed with Synapse (should work with other exploits though)
---]]
-
-print("-- Chat Spy Executed --")
-print("Type \"spy\" to enable or disable the chat spy.")
-print("Only tested if this works executed with Synapse (should work with other exploits though)")
-print("https://github.com/dehoisted/Chat-Spy")
-
--- Config
 Config = {
 	enabled = true,
-	spyOnMyself = true,
+	spyOnMyself = false,
 	public = false,
 	publicItalics = true
 }
 
--- Customizing Log Output
 PrivateProperties = {
 	Color = Color3.fromRGB(0,255,255); 
 	Font = Enum.Font.SourceSansBold;
